f20a5fe7f2077585bd42c8708a400d52ea0faa1d
oldjerry-macmini <1569918495@qq.com>
2026-08-30 13:16:20 +0800

    feat(i18n): translate LCR meter

---
+++ apps-tools/lcr_meter/locales/map.js
@@ -0,0 +1,31 @@
+(function () {
+    "use strict";
+    window.LCR_I18N_TEXT_MAP = {
+        "Device is already in use by another user.": "lcr.device_busy",
+        "Connection with device is lost.": "lcr.connection_lost",
+        "SETTINGS": "lcr.settings", "RESET": "lcr.reset", "LOG DATA": "lcr.log_data",
+        "STOP": "lcr.stop", "HOLD": "lcr.hold", "START": "lcr.start",
+        "P. DISP:": "lcr.primary_display", "S. DISP:": "lcr.secondary_display",
+        "MODE:": "lcr.mode", "FREQ.:": "lcr.frequency", "SHUNT:": "lcr.shunt",
+        "MIN:": "lcr.minimum", "MAX:": "lcr.maximum", "AVG:": "lcr.average",
+        "Tol:": "lcr.tolerance_short", "Rel:": "lcr.relative_short",
+        "DATA OPTIONS": "lcr.data_options", "MEASURMENT OPTIONS": "lcr.measurement_options",
+        "AUTO": "lcr.auto", "Series": "lcr.series", "Parallel": "lcr.parallel",
+        "DISPLAY OPTIONS": "lcr.display_options", "Manual": "lcr.manual",
+        "Auto": "lcr.auto", "Relative": "lcr.relative", "Tolerance": "lcr.tolerance",
+        "DATE": "lcr.date", "Export Table": "lcr.export", "Clear": "lcr.clear",
+        "Starting calibration process...": "lcr.calibration_start",
+        "Please connect open calibration circuit and press start": "lcr.connect_open",
+        "Calibrating....": "lcr.calibrating", "Calibrating.....": "lcr.calibrating",
+        "Please connect short calibration and press next": "lcr.connect_short",
+        "Start": "lcr.start", "Next": "lcr.next",
+        "You have successfully calibrated your Red Pitaya.": "lcr.calibrated",
+        "Finish": "lcr.finish", "Unable to load application.": "lcr.load_failed",
+        "E3: Socket not opened. Press Reload to reload the application.": "lcr.socket_not_open",
+        "Reload": "lcr.reload", "Time-out.": "lcr.timeout",
+        "Socket closed. Press reload to reload the application.": "lcr.socket_closed",
+        "Extension module status": "lcr.extension_status",
+        "Extension module is not connected": "lcr.extension_missing",
+        "Close": "lcr.close", "I2C-tool not installed": "lcr.i2c_missing", "Install": "lcr.install"
+    };
+}());
