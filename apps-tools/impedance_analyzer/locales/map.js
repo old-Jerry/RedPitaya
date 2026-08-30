2bfc50b139e7e67adbc19b76f4e45af0145026b0
oldjerry-macmini <1569918495@qq.com>
2026-08-30 13:18:44 +0800

    feat(i18n): translate impedance analyzer

---
+++ apps-tools/impedance_analyzer/locales/map.js
@@ -0,0 +1,26 @@
+(function () {
+    "use strict";
+    window.IA_I18N_TEXT_MAP = {
+        "Device is already in use by another user.": "ia.device_busy",
+        "Connection with device is lost.": "ia.connection_lost",
+        "Calibrating...": "ia.calibrating", "INFO": "ia.info", "MENU": "ia.menu",
+        "Export Graph": "ia.export_graph", "Export in CSV": "ia.export_csv", "RESET": "ia.reset",
+        "RUN": "ia.run", "STOP": "ia.stop", "Close": "ia.close", "Export": "ia.export",
+        "Content": "ia.content", "Settings": "ia.settings", "Content settings": "ia.content_settings",
+        "Plot settings": "ia.plot_settings", "CURSORS": "ia.cursors", "LCR extension": "ia.lcr_extension",
+        "Start frequency [Hz]": "ia.start_frequency", "End frequency [Hz]": "ia.end_frequency",
+        "Steps": "ia.steps", "Averaging": "ia.averaging", "Amplitude [V]": "ia.amplitude",
+        "DC bias [V]": "ia.dc_bias", "Measure scale": "ia.measure_scale",
+        "Lin": "ia.linear_short", "Log": "ia.log_short", "Shunt [Ω]": "ia.shunt",
+        "Y-axis": "ia.y_axis", "X-axis scale": "ia.x_axis_scale",
+        "LINEAR": "ia.linear", "LOGARITHMIC": "ia.logarithmic", "Cursor": "ia.cursor",
+        "Measuring...": "ia.measuring", "Current step:": "ia.current_step",
+        "Current freq:": "ia.current_frequency", "Frequency [Hz]": "ia.frequency",
+        "Connection diagram": "ia.connection_diagram", "Calibration": "ia.calibration",
+        "To calibrate your setup connect IN2 to IN1 (bypass DUT) and press CALIBRATE.": "ia.calibration_help",
+        "Calibrate": "ia.calibrate", "Reset Calibration": "ia.reset_calibration",
+        "Oops, something went wrong": "ia.error_title",
+        "Please send us the report and help us to fix this issue": "ia.error_request",
+        "Thank you!": "ia.thanks", "Send report": "ia.send_report", "Restart Application": "ia.restart"
+    };
+}());
